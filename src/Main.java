6eae3ed619095c458019452f8077f9990a1fd84b
mike <tatarincevmisha@gmail.com>
2026-04-04 11:52:34 +0300

    worked

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+//package com.laba1_OOP_JAVA;
+public class Main {
+
+    public static void main(String[] args) {
+        short[][] matrix_a = {{4,1,9},{5,0,2},{7,8,3}};
+        short[][] matrix_b = {{8,3,4},{7,1,5},{0,3,8}};
+
+        Matrix_operations operation = new Matrix_operations();
+            short[][] matrix_c = operation.xor(matrix_a, matrix_b);
+            int summa_min_c = operation.sum(matrix_c);
+        System.out.print("Результат матриці А ⊕ матриці В:");
+        for(int i = 0; i < matrix_c.length; i++){
+            System.out.println();
+            for(int j = 0; j < matrix_c[0].length; j++){
+                System.out.print(" " + matrix_c[i][j]);
+            }
+        }
+
+        System.out.println("\nСумма найменших елементів кожного рядка матриці С(А ⊕ B): " + summa_min_c);
+    }
+}
---
+++ src/Matrix_operations.java
@@ -0,0 +1,28 @@
+
+public class Matrix_operations {
+    private int sum_element;
+    private short[][] matrix_c;
+    public short[][] xor(short[][] matrix_a, short[][] matrix_b){
+        matrix_c = new short[matrix_a.length][matrix_a[0].length]; // new - виділяю місце в пам'яті для нового об'єкту, щоб я міг туди класти числа(поки в матриці в мене всі 0)
+        for(int i = 0;  i < matrix_a.length; i++){
+            for(int j = 0; j < matrix_a[i].length; j++){
+                matrix_c[i][j] = (short) (matrix_a[i][j] ^ matrix_b[i][j]);
+            }
+        }
+        return matrix_c;
+    }
+
+    public int sum(short[][] matrix){
+        sum_element = 0;
+        for(int i = 0; i < matrix.length; i++){
+            int min = matrix[i][0];
+            for(int j = 1; j < matrix[0].length; j++){ // зап за matrix[0].length або matrix_a[i].length
+                if (matrix[i][j] < min){
+                    min = matrix[i][j];
+                }
+            }
+            sum_element += min;
+        }
+        return sum_element;
+    }
+}
